acb0d728748a4e29e1f17621f069fadb9bcc73c5
Obehi Iredia <44153313+i0bj@users.noreply.github.com>
2021-04-12 22:13:26 -0400

    Add SMTP VRFY py script from OSCP studies

---
+++ SMTP_VRFY.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import socket
+import sys
+
+if len(sys.argv) != 2
+    print("Usage: vrfy.py <username>")
+    sys.exit(0)
+    
+#New socekt for connection to SMTP server    
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#Connect to SMTP server
+connect = s.connect(("SMTP server IP",25))
+
+#Receiving banner
+banner = s.recv(1024)
+
+print(banner)
+
+#Send the command VRFY to have the server verify an email address
+s.send("VRFY" + sys.argv[1] + "\r\n")
+result = s.recv(1024)
+
+print(result)
+
+#Close connection.
+s.close()
