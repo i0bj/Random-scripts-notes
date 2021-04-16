af605bbf383decdbbfe8c6bcfb47ee4fd48ea5a4
Obehi Iredia <44153313+i0bj@users.noreply.github.com>
2021-04-15 21:50:49 -0400

    Update to read usernames from txt file.

--- SMTP_VRFY.py
+++ SMTP_VRFY.py
@@ -4,24 +4,23 @@ import socket
 import sys
 
 if len(sys.argv) != 2
-    print("Usage: vrfy.py <username>")
+    print("Usage: vrfy.py <Enter txt file containing usernames>")
     sys.exit(0)
     
-#New socekt for connection to SMTP server    
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ with open(sys.argv[1]) as f:
+        hosts = f.read().splitlines()
+        
+#New socekt for connection to SMTP server
+for i in hosts:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Connect to SMTP server
-connect = s.connect(("SMTP server IP",25))
-
+    connect = s.connect(("SMTP server IP",25))
 #Receiving banner
-banner = s.recv(1024)
-
-print(banner)
-
+    banner = s.recv(1024)
+    print(banner)
 #Send the command VRFY to have the server verify an email address
-s.send("VRFY" + sys.argv[1] + "\r\n")
-result = s.recv(1024)
-
-print(result)
-
+    s.send("VRFY" + sys.argv[1] + "\r\n")
+    result = s.recv(1024)
+    print(result)
 #Close connection.
-s.close()
+    s.close()
