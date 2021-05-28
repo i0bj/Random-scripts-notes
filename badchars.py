7d34fc9cebf65d6e1b5132e5f5c488dfbc0aa3eb
Obehi Iredia <44153313+i0bj@users.noreply.github.com>
2021-05-28 01:10:24 -0400

    Update badchars.py

--- badchars.py
+++ badchars.py
@@ -1,4 +1,4 @@
 import sys
-#Simply prints hex charaters which can be used to find bad characters when developing your exploit.
+#Simply prints hex characters which can be used to find bad characters when developing your exploit.
 for x in range(0, 256):
   sys.stdout.write("\\x" + '{:02x}'.format(x))
